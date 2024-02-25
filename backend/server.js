c36f5d332149e6e80dbed25b7d00752b68a46584
Willileo <97127192+Willileo24@users.noreply.github.com>
2024-02-25 12:24:55 +0100

    Combined backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressSession = require('express-session');
 const log4js = require('log4js');
 const passport = require('passport');
+const path = require('path');
 const { ensureAuthenticated } = require('./auth/authenticate');
 
 require('dotenv').config();
@@ -46,6 +47,12 @@ app.use('/api/system', ensureAuthenticated, require('./routes/system'));
 app.use('/api/nfc', require('./routes/nfc').router);
 app.use('/auth', require('./routes/auth'));
 app.use('/public', require('./routes/public'));
+
+// Serve frontend
+app.use('/', ensureAuthenticated, express.static(path.join(__dirname, 'frontendBuild')));
+app.use('/*', ensureAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontendBuild', 'index.html'));
+});
 
 let server = app.listen(process.env.PORT, () => {
     logger.info(`Server is running on port ${process.env.port}`);
